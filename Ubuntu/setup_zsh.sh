aa00d181d20fdbf2c230030e4d3966c4b4b4c19b
Kasun Herath <76202791+kasun-97@users.noreply.github.com>
2023-10-07 22:10:38 +0530

    zsh: Go away Oh My Zsh

--- Ubuntu/setup_zsh.sh
+++ Ubuntu/setup_zsh.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 
 echo "Install ZSH"
-sudo apt install -y zsh-autosuggestions zsh-syntax-highlighting zsh
+sudo apt install zsh -y
 
-echo "Install Oh my ZSH"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Clone plugins"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.zsh/plugins/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.zsh/plugins/zsh-syntax-highlighting
+wget -P $HOME/.zsh/plugins https://raw.githubusercontent.com/rupa/z/master/z.sh
 
-echo "Install plugins"
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+echo "Get the dotfiles"
+wget -P $HOME/.zsh https://raw.githubusercontent.com/kasun-97/dotfiles/master/.zsh/.zshrc 
+wget -P $HOME/.zsh https://raw.githubusercontent.com/kasun-97/dotfiles/master/.zsh/.aliases
 
-echo "Now add plugins to ~/.zshrc line below"
-echo "plugins=(git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting zsh-autocomplete)"
-echo "reload zsh config with 'source ~/.zshrc`"
-echo "You may want to switch to zsh from bash if you already have not by running 'chsh -s $(which zsh)'"
-echo "You can reassign tab key behaviour to cycle through selection list by running the below command:"
-echo "bindkey '\t' menu-complete "$terminfo[kcbt]" reverse-menu-complete"
+echo "Create a symbolic for our custom .zshrc location"
+rm ~/.zshrc #remove the current config file if it excist.
+ln -s -T ~/.zsh/.zshrc ~/.zshrc
+
+echo "Swtich to ZSH"
+chsh -s /bin/zsh
 
 # Reference:
 # https://github.com/marlonrichert/zsh-autocomplete#configuration
