af0d06e0859d3cc526057ddc63a2f1afa5a62ec6
Kasun Herath <76202791+kasun-97@users.noreply.github.com>
2023-10-13 12:28:45 +0530

    Update setup_build_envirionment-ubuntu.sh

--- android/setup_build_envirionment-ubuntu.sh
+++ android/setup_build_envirionment-ubuntu.sh
@@ -2,7 +2,7 @@
 sudo apt update
 
 echo -e "Installing some extras"
-sudo apt install -y neofetch tmate ranger bash-completion neovim
+sudo apt install -y tmux tmate ranger neovim
 
 echo -e "Installing build essentials"
 sudo apt install -y \
