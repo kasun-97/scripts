83770dc9678e0a386fdbc0068969f2baddb9b7df
Kasun Herath <76202791+kasun-97@users.noreply.github.com>
2023-11-30 09:23:23 +0530

    zsh: Switch to oh-my-zsh
    + some clean up

--- Ubuntu/setup_zsh.sh
+++ Ubuntu/setup_zsh.sh
@@ -3,22 +3,30 @@
 echo "Install ZSH"
 sudo apt install zsh git -y
 
+echo "Install oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 echo "Clone plugins"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.zsh/plugins/powerlevel10k
-git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.zsh/plugins/zsh-syntax-highlighting
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $HOME/.zsh/plugins/zsh-autocomplete
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 wget -P $HOME/.zsh/plugins https://raw.githubusercontent.com/rupa/z/master/z.sh
 
-echo "Get the dotfiles"
-wget -P $HOME/.zsh https://raw.githubusercontent.com/kasun-97/dotfiles/master/.zsh/.zshrc 
-wget -P $HOME/.zsh https://raw.githubusercontent.com/kasun-97/dotfiles/master/.zsh/.aliases
+echo "Find 'plugins=(git)' in .zshrc and"
+echo "replace it with"
+echo "plugins=(git zsh-autosuggestions zsh-syntax-highlighting z)"
 
-echo "Create a symbolic for our custom .zshrc location"
-rm ~/.zshrc #remove the current config file if it excist.
-ln -s -T ~/.zsh/.zshrc ~/.zshrc
+echo "To enable powerlevel10k"
+echo "Set ZSH_THEME=\"powerlevel10k/powerlevel10k\" in ~/.zshrc"
 
-echo "Swtich to ZSH"
+echo "Get aliases dotfile"
+wget -P $HOME/.oh-my-zsh https://raw.githubusercontent.com/kasun-97/dotfiles/master/.zsh/.aliases
+
+echo "To enable aliases, add below lines in .zshrc"
+echo "# Aliases"
+echo "source $ZSH/.aliases"
+
+# Swtich to zsh
 chsh -s /bin/zsh
 
 # Reference:
