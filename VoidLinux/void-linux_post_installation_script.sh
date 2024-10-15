708adc7b63852d8e5eb5ba340203b6f7a6dbaaad
Kasun Herath <76202791+kasun-97@users.noreply.github.com>
2024-10-15 22:52:53 +0530

    Install AMD graphics drivers

--- VoidLinux/void-linux_post_installation_script.sh
+++ VoidLinux/void-linux_post_installation_script.sh
@@ -7,3 +7,12 @@ sudo xbps-install -Suy
 
 # Add non free and multilib repositories
 sudo xbps-install -y void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
+
+# Install AMD graphics drivers
+sudo xbps-install -y linux-firmware-amd 
+sudo xbps-install -y mesa-dri 
+sudo xbps-install -y vulkan-loader
+sudo xbps-install -y mesa-vulkan-radeon 
+sudo xbps-install -y xf86-video-amdgpu # for Xorg
+sudo xbps-install -y mesa-vaapi
+sudo xbps-install -y mesa-vdpau
