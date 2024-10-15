09b20f55019bda8bd4bb67c63ba5ac91835c1505
Kasun Herath <76202791+kasun-97@users.noreply.github.com>
2024-10-15 22:57:59 +0530

    Clean the code and configure bluetooth service

--- VoidLinux/void-linux_post_installation_script.sh
+++ VoidLinux/void-linux_post_installation_script.sh
@@ -28,9 +28,14 @@ sudo mkdir -p /etc/pipewire/pipewire.conf.d
 sudo ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
 sudo ln -s /usr/share/examples/pipewire/20-pipewire-pulse.conf /etc/pipewire/pipewire.conf.d/
 
-# Bluetooth audio 
-sudo xbps-install -y libspa-bluetooth
 # ASLA integration
 # sudo mkdir -p /etc/alsa/conf.d
 # sudo ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d
 # sudo ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d
+
+# Bluetooth audio 
+sudo xbps-install -y libspa-bluetooth
+
+# Configure bluetooth service 
+sudo xbps-install -y bluez
+sudo ln -s /etc/sv/bluetoothd /var/service/
